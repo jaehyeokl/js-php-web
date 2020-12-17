f5e1f7e6f7826b8be737f1ee549b83ebb1681de2
root <root@ip-172-31-44-169.ap-northeast-2.compute.internal>
2020-12-17 11:04:49 +0000

    디렉토리 구조 설정

---
+++ html/index.php
@@ -0,0 +1,17 @@
+<!-- Database 'portfolio' 연결 -->
+<?php
+    $servername = "localhost";
+    $port = "3306";
+    $dbname = "portfolio";
+    $user = "manager";
+    $password = "8fuch#CJjcyW8zHgLISW27^J8D4NB4VI^fLMp4XtR0ZO7!4u&k";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $user, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "connect!";
+    } catch (PDOException $ex) {
+        echo "failed! : ".$ex->getMessage()."<br>";
+    }
+?>
