e1a70957b3d23aecca2f66871f1f074c5c05039b
root <root@ip-172-31-44-169.ap-northeast-2.compute.internal>
2020-12-19 05:27:53 +0000

    상단 메뉴 설정

--- html/index.php
+++ html/index.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="css/index.css">
     <!-- <script src="https://kit.fontawesome.com/8451689280.js" crossorigin="anonymous"></script> -->
 </head>
+
     <!-- Header -->
     <section id="header">
         <div class="header container">
@@ -19,7 +20,8 @@
                     <ul>
                         <li><a href="#main">Home</a></li>
                         <li><a href="#projects">Projects</a></li>
-                        <li><a href="#about">About</a></li>
+                        <li><a href="#blog">Blog</a></li>
+                        <!-- <li><a href="#about">About</a></li> -->
                         <li><a href="#contact">Contact</a></li>
                     </ul>
                 </div>
@@ -27,7 +29,6 @@
         </div>
     </section>
     <!-- End Header -->
-
 
     <!-- Main Section  -->
     <section id="main">
@@ -41,118 +42,40 @@
     </section>
     <!-- End Main Section  -->
 
-    <!-- Service Section -->
-    <!-- <section id="services">
-        <div class="services container">
-            <div class="service-top">
-                <h1 class="section-title">Serv<span>i</span>ces</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum deleniti maiores pariatur assumenda quas magni et, doloribus quod voluptate quasi molestiae magnam officiis dolorum, dolor provident atque molestias voluptatum explicabo!</p>
-            </div>
-            <div class="service-bottom">
-                <div class="service-item">
-                    <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png"/></div>
-                    <h2>Web Design</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed architecto placeat beatae tenetur officia quod</p>
-                </div>
-                <div class="service-item">
-                    <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png"/></div>
-                    <h2>Web Design</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed architecto placeat beatae tenetur officia quod</p>
-                </div>
-                <div class="service-item">
-                    <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png"/></div>
-                    <h2>Web Design</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed architecto placeat beatae tenetur officia quod</p>
-                </div>
-                <div class="service-item">
-                    <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/services.png"/></div>
-                    <h2>Web Design</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis debitis rerum, magni voluptatem sed architecto placeat beatae tenetur officia quod</p>
-                </div>
-            </div>
-        </div>
-    </section> -->
-    <!-- End Service Section -->
-
     <!-- Projects Section -->
     <section id="projects">
         <div class="projects container">
             <div class="projects-header">
                 <h1 class="section-title">Recent <span>Projects</span></h1>
             </div>
-            <!-- <div class="all-projects">
-                <div class="project-item">
-                <div class="project-info">
-                    <h1>Project 1</h1>
-                    <h2>Coding is Love</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
-                </div>
-                <div class="project-img">
-                    <img src="./img/img-1.png" alt="img">
-                </div>
-                </div>
-                <div class="project-item">
-                <div class="project-info">
-                    <h1>Project 2</h1>
-                    <h2>Coding is Love</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
-                </div>
-                <div class="project-img">
-                    <img src="./img/img-1.png" alt="img">
-                </div>
-                </div>
-                <div class="project-item">
-                <div class="project-info">
-                    <h1>Project 3</h1>
-                    <h2>Coding is Love</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
-                </div>
-                <div class="project-img">
-                    <img src="./img/img-1.png" alt="img">
-                </div>
-                </div>
-                <div class="project-item">
-                <div class="project-info">
-                    <h1>Project 4</h1>
-                    <h2>Coding is Love</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
-                </div>
-                <div class="project-img">
-                    <img src="./img/img-1.png" alt="img">
-                </div>
-                </div>
-                <div class="project-item">
-                <div class="project-info">
-                    <h1>Project 5</h1>
-                    <h2>Coding is Love</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto cupiditate voluptatum impedit unde rem ipsa distinctio illum quae mollitia ut, accusantium eius odio ducimus illo neque atque libero non sunt harum? Ipsum repellat animi, fugit architecto voluptatum odit et!</p>
-                </div>
-                <div class="project-img">
-                    <img src="./img/img-1.png" alt="img">
-                </div>
-            </div> -->
-            </div>
         </div>
     </section>
     <!-- End Projects Section -->
 
-  <!-- About Section -->
-  <section id="about">
-    <div class="about container">
-      <div class="col-left">
-        <div class="about-img">
-          <img src="./img/img-2.png" alt="img">
+    <!-- Blog Section -->
+    <section id="blog">
+        <div class="blog container">
+            <div class="section-title">Blog</div>
         </div>
-      </div>
-      <div class="col-right">
-        <h1 class="section-title">About <span>me</span></h1>
-        <h2>Front End Developer</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, velit alias eius non illum beatae atque repellat ratione qui veritatis repudiandae adipisci maiores. At inventore necessitatibus deserunt exercitationem cumque earum omnis ipsum rem accusantium quis, quas quia, accusamus provident suscipit magni! Expedita sint ad dolore, commodi labore nihil velit earum ducimus nulla quae nostrum fugit aut, deserunt reprehenderit libero enim!</p>
-        <a href="#" class="cta">Download Resume</a>
-      </div>
-    </div>
-  </section>
-  <!-- End About Section -->
+    </section>
+    <!-- End Blog Section  -->
+
+    <!-- About Section -->
+    <section id="about">
+        <div class="about container">
+            <div class="col-left">
+                <div class="about-img">
+                    <img src="./img/img-2.png" alt="img">
+                </div>
+            </div>
+            <div class="col-right">
+                <h1 class="section-title">About <span>me</span></h1>
+                <h2>Front End Developer</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, velit alias eius non illum beatae atque repellat ratione qui veritatis repudiandae adipisci maiores. At inventore necessitatibus deserunt exercitationem cumque earum omnis ipsum rem accusantium quis, quas quia, accusamus provident suscipit magni! Expedita sint ad dolore, commodi labore nihil velit earum ducimus nulla quae nostrum fugit aut, deserunt reprehenderit libero enim!</p>
+            </div>
+        </div>
+    </section>
+    <!-- End About Section -->
 
   <!-- Contact Section -->
   <section id="contact">
