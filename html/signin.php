92746dc3766a345f987da7602ccfe2d61d87060c
root <root@ip-172-31-44-169.ap-northeast-2.compute.internal>
2021-01-01 15:12:44 +0900

    로그인 입력 이메일/ 비밀번호 양식 체크

--- html/signin.php
+++ html/signin.php
@@ -35,14 +35,53 @@
         <div class="signin body">
             <form class="form" action="signin.php" method="post">
                 <span class="form__text">email</span>
-                <input class="form__input" name="email" type="email" maxlength="40" id="email">
+                <input class="form__input email" name="email" type="email" maxlength="40">
                 <span class="form__text email-rule" id="testid">올바른 이메일을 입력해주세요</span>
                 <span class="form__text">password</span>
-                <input name="password" type="password" minlength="8" maxlength="20" class="form__input" id="password">
+                <input class="form__input password" name="password" type="password" minlength="8" maxlength="20">
                 <!-- <a class="form__button_forgot" type="button" href="#">비밀번호를 잊으셨나요?</a> -->
                 <button name="submit" type="button" class="form__button" id="submit">로그인</button>
             </form>
         </div>
     </section>
     <!-- End Sign in Section  -->
+
+    <script>
+        // 올바른 이메일 형식 / 비밀번호 길이 확인
+
+        // 정규표현식을통한 이메일 형식 체크 (ex email@naver.com)
+        // 이메일 양식과 일치하지 않을 경우 숨겨져있는 안내메세지를 보여준다
+        let emailRule = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+        let inputEmail = document.querySelector('.form__input.email');
+        inputEmail.addEventListener("keyup", checkEmail);
+
+        function checkEmail() {
+            const email = inputEmail.value;
+            const checkRule = emailRule.test(email)
+                
+            if (email.length > 0) {
+                if (checkRule==true) {
+                    document.querySelector('.email-rule').style.visibility = "hidden";
+                    return true;
+                } else {
+                    document.querySelector('.email-rule').style.visibility = "visible";
+                    return false;
+                }
+            } else {
+                document.querySelector('.email-rule').style.visibility = "hidden";
+            }
+        }
+            
+        // 올바른 이메일 양식, 비밀번호 길이를 입력했을때, 로그인 버튼을 활성화한다
+        const submitButton = document.querySelector('#submit');
+        submitButton.addEventListener("click", checkInput);
+        function checkInput() {
+            const statusEmail = checkEmail();
+            const passwordLength = document.querySelector('.form__input.password').value.length;
+
+            if (statusEmail == true && passwordLength >= 8) {
+                submitButton.type = "submit";
+            }
+        }
+    </script>
 </html>
