278d047374c4988b3c0149e85ac86845551d8dfc
root <root@ip-172-31-44-169.ap-northeast-2.compute.internal>
2021-01-11 20:49:41 +0900

    LIVE 메뉴 추가 및 스트림 테스트

--- html/test.php
+++ html/test.php
@@ -5,6 +5,7 @@
 <!doctype html>
 <html>
     <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
         <title>Dash.js Rocks</title>
         <style>
             video {
@@ -15,15 +16,23 @@
     </head>
     <body>
         <div>
-            <video id="videoPlayer" controls></video>
+            <!-- <video id="videoPlayer" controls></video> -->
+            <!-- <video>
+                <source src="/video/streaming/my_video_manifest.mpd">
+                <source src="/video/streaming/audio.webm">
+            </video> -->
+            <video data-dashjs-player autoplay controls src="/video/streaming/my_video_manifest.mpd">
+                <source src="/video/streaming/audio.webm">
+            </video>
+            <script src="http://cdn.dashjs.org/latest/dash.all.min.js"></script>
         </div>
-        <script src="http://cdn.dashjs.org/latest/dash.all.min.js"></script>
+        <!-- <script src="http://cdn.dashjs.org/latest/dash.all.min.js"></script>
         <script>
             (function(){
-                var url = "https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd";
+                var url = "/video/project/test1.mpd";
                 var player = dashjs.MediaPlayer().create();
                 player.initialize(document.querySelector("#videoPlayer"), url, true);
             })();
-        </script>
+        </script> -->
     </body>
 </html>
