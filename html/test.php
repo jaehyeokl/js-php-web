cb6b6ef5931a0ac9c5acc86bef13bf9e94ce33a1
root <root@ip-172-31-44-169.ap-northeast-2.compute.internal>
2021-01-11 14:16:11 +0900

    DASH.js 테스트 작성

--- html/test.php
+++ html/test.php
@@ -1,48 +1,29 @@
 <?php
-    $request = apache_request_headers();
-    $response = apache_response_headers();
-    // $headers = getallheaders();
-    // echo $headers;
-    // var_dump($request);
-    // echo '<br>';
-    // var_dump($response);
-    // echo '<br>';
-    // var_dump($_SERVER);
-
-    // echo $_SERVER['HTTP_USER_AGENT'] ."<br>"."dd";
-
-    // $browser = get_browser(null, true);
-    // echo $browser['platform'];
-    // echo "<br>";
-    // echo $browser['browser'];
-    // echo "<br>";
-
-    // $details = json_decode(file_get_contents("http://ipinfo.io/"));
-    //     echo $details->country; // 어느나라니
-    //     echo $details->city; // 어느도시니​
-
-    // $a = 1;
-
-    // if ($a == 1) {
-    //     session_cache_expire(1);
-    //     session_start();
-    //     $_SESSION['test'] = 1;
-    //     echo "a = 1";
-    //     echo session_cache_expire();
-
-    //     $a++;
-    //     echo $a;
-    // } else {
-    //     echo $_SESSION['test'];
-    //     echo $_SESSION['332423'];
-    // }
-
-
-    // $today = date('Y-2-t');
-    // echo $today;
-
-    // $montly = date('Y-'.'0'.$i);
-
-        // phpinfo();
     
 ?>
+
+<!doctype html>
+<html>
+    <head>
+        <title>Dash.js Rocks</title>
+        <style>
+            video {
+                width: 640px;
+                height: 360px;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <video id="videoPlayer" controls></video>
+        </div>
+        <script src="http://cdn.dashjs.org/latest/dash.all.min.js"></script>
+        <script>
+            (function(){
+                var url = "https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd";
+                var player = dashjs.MediaPlayer().create();
+                player.initialize(document.querySelector("#videoPlayer"), url, true);
+            })();
+        </script>
+    </body>
+</html>
