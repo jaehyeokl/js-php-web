570793ec9ea08cac1fdb0982091cb3ca73c33a30
root <root@ip-172-31-44-169.ap-northeast-2.compute.internal>
2021-01-21 20:24:59 +0900

    live.php 주석제거 및 코드정리

--- html/live.php
+++ html/live.php
@@ -1,159 +1,11 @@
 <?php 
-    define("IV","0123456789abcdef"); // DRM policy(정책)을 암호화할때 사용되는 상수
-    include_once("../resources/drm_config.php"); // PallyCon DRM key
+    // 라이브 스트리밍 페이지
+    // 이메일 인증을 하면 라이브 스트리밍을 바로 시청할 수 있다
 
-
-    
-    $policyDetail = array (
-        'policy_version' => 2,
-        'playback_policy' => array (
-            'persistent' => false, // 오프라인 라이센스 적용 X
-            'license_duration' => 0,  // 300
-            // 'expire_date' => '<yyyy-mm-ddThh:mm:ssZ 형식의 만료 시간(GMT)>', // 위의 license_duration 과 함께 사용할 수 없다
-            // 'rental_duration' => 1,
-            // 'playback_duration' => 1,
-            'allowed_track_types' => 'ALL', // ??
-        ),
-        'security_policy' => array (
-            0 => array (
-                'track_type' => 'ALL',
-                'widevine' => array (
-                    'security_level' => 1,
-                    'required_hdcp_version' => 'HDCP_NONE',
-                    // 'required_cgms_flags' => 'CGMS_NONE',
-                    // 'disable_analog_output' => false,
-                    // 'hdcp_srm_rule' => 'HDCP_SRM_RULE_NONE',
-                ),
-                // 'playready' => array (
-                    //     'security_level' => 150,
-                    //     'digital_video_protection_level' => 100,
-                    //     'analog_video_protection_level' => 100,
-                    //     'digital_audio_protection_level' => 100,
-                    //     'require_hdcp_type_1' => true,
-                    // ),
-                    // 'fairplay' => array (
-                        //     'hdcp_enforcement' => -1,
-                        //     'allow_airplay' => true,
-                        //     'allow_av_adapter' => true,
-                        // ),
-                        // 'ncg' => array (
-                            //     'allow_mobile_abnormal_device' => true,
-                            //     'allow_external_display' => true,
-                            //     'control_hdcp' => 0,
-                            // ),
-                        ),
-                    ),
-                    // 'external_key' => array (
-                        //     'mpeg_cenc' => array (
-                            //         0 => array (
-                                //             'track_type' => 'ALL',
-                                //             'key_id' => '<hex-string>',
-                                //             'key' => '<hex-string>',
-                                //             'iv' => '<hex-string>',
-                                //         ),
-                                //     ),
-                                //     'hls_aes' => array (
-                                    //         0 => array (
-                                        //             'track_type' => 'ALL',
-                                        //             'key' => '<hex-string>',
-                                        //             'iv' => '<hex-string>',
-                                        //         ),
-                                        //     ),
-                                        //   'ncg' => array (
-                                            //         'cek' => '<hex-string>',
-                                            //     ),
-                                            // ),
-    );
-
-    // 정책 생성
-    $policy = array (
-        'policy_version' => 2,
-        'playback_policy' => 
-        array (
-            'persistent' => false,
-            'license_duration' => 0,
-            'rental_duration' => 0,
-            'playback_duration' => 0,
-            'allowed_track_types' => 'ALL',
-        ),
-        'security_policy' => 
-        array (
-            0 => 
-            array (
-                'track_type' => 'ALL',
-                'widevine' => 
-                array (
-                    'security_level' => 1,
-                    'required_hdcp_version' => 'HDCP_NONE',
-                    'required_cgms_flags' => 'CGMS_NONE',
-                    'disable_analog_output' => false,
-                    'hdcp_srm_rule' => 'HDCP_SRM_RULE_NONE',
-                ),
-                'playready' => 
-                array (
-                    'security_level' => 150,
-                    'digital_video_protection_level' => 100,
-                    'analog_video_protection_level' => 100,
-                    'digital_audio_protection_level' => 100,
-                    'require_hdcp_type_1' => false,
-                ),
-                'fairplay' => 
-                array (
-                    'hdcp_enforcement' => -1,
-                    'allow_airplay' => true,
-                    'allow_av_adapter' => true,
-                ),
-                'ncg' => 
-                array (
-                    'allow_mobile_abnormal_device' => false,
-                    'allow_external_display' => false,
-                    'control_hdcp' => 0,
-                ),
-            ),
-        ),
-    );
-
-    $samplePolicy = array (
-        'playback_policy' => 
-        array (
-          'limit' => true,
-          'persistent' => false,
-          'duration' => 3600,
-        ),
-    );
-
-    $samplePolicy2 = array (
-        'policy_version' => 2,
-    );
-    
-    // 정책을 AES256 으로 암호화
-    $policyString = openssl_encrypt(json_encode($policy), "AES-256-CBC", $siteKey, 0, IV);
-
-    // 해쉬생성
-    $drmType = "Widevine";
-    $userId = "LICENSETOKEN"; // 없을경우의 default 값
-    $cid = "test4";
-    $timestamp = gmdate("Y-m-d\Th:i:s\Z");
-
-    $body = $accessKey.$drmType.$siteId.$userId.$cid.$policyString.$timestamp;
-    $hashString = base64_encode(hash("sha256", $body, true));
-
-
-    // 라이센스 요청을 위한 토큰 생성
-    // 앞에서 암호화한 정책과, 해쉬를 포함한 json 을 통해 생성한다
-    $tokenData = array (
-        'drm_type' => $drmType,
-        'site_id' => $siteId,
-        'user_id' => $userId,
-        'cid' => $cid,
-        'token' => $policyString,
-        'timestamp' => $timestamp,
-        'hash' => $hashString,
-        'response_format' => 'original', // default
-        'key_rotation' => false // default
-    );
-
-    $token = base64_encode(json_encode($tokenData));
+    include_once("../resources/config.php");
+    $signinSessionStatus = checkSigninStatus(); // 로그인 세션 확인
+    $connectDB = connectDB(); // DB 연결
+    logVisitor(); // 방문 로그 체크 및 저장
 ?>
 
 <!DOCTYPE html>
@@ -209,8 +61,9 @@
                 </div>
                 <div class="header-auth">
                     <div class="auth-user certified">
+                        <span>USER</span>
                         <span class="certified-email"></span>
-                        <span class="certified-drmType"></span>
+                        <span class="certified-drmtype"></span>
                     </div>
                     <div class="auth-guide">
                         <span class="auth-message">라이브 시청을 위해서는 이메일 인증이 필요합니다</span>
@@ -230,6 +83,7 @@
             </div>
         </div>
     </section>
+    <!-- End Live Section -->
 
     <!-- User certify -->
     <div class="popup">
@@ -248,49 +102,7 @@
         </form>
     </div>
     <!-- End User certify -->
-
     
+    <!-- live.js 유저 인증 및 플레이어 재생 -->
     <script src="js/live.js"></script>
-    <script>
-        
-
-    
-        
-        // 플레이에어서 라이센스 DRM 연동하기!
-        // let player = videojs("video");
-        
-        // const token = '<?= $token ?>';
-
-        // player.ready(function(){
-        //     player.src({
-        //         'src': '/video/project/dash/stream.mpd',
-        //         'type': 'application/dash+xml',
-        //         'keySystemOptions': [
-        //             {
-        //                 'name': 'com.widevine.alpha',
-        //                 'options':{
-        //                     'serverURL' : 'https://license.pallycon.com/ri/licenseManager.do',
-        //                     'httpRequestHeaders' : {
-        //                         'pallycon-customdata-v2' : licenseToken,
-        //                         // 'pallycon-customdata-v2' : token,
-        //                     }
-        //                 }
-        //             },
-        //             {
-        //                 'name': 'com.microsoft.playready',
-        //                 'options':{
-        //                     'serverURL' : 'https://license.pallycon.com/ri/licenseManager.do',
-        //                     'httpRequestHeaders' : {
-        //                         'pallycon-customdata-v2' : licenseToken,
-        //                         // 'pallycon-customdata-v2' : token,
-        //                     }
-        //                 }
-        //             }
-        //         ]
-        //     });
-        // })
-        // player.play();
-    </script>
-    <!-- End Live Section -->
-
 </body>
